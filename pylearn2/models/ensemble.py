1ac2e4e8415fc1ed813933cbe2ad6dafd57622de
skearnes <kearnes@stanford.edu>
2014-05-15 14:48:25 -0700

    As MLP layer

--- pylearn2/models/ensemble.py
+++ pylearn2/models/ensemble.py
@@ -1,82 +1,91 @@
 """
-Ensemble models.
+Ensemble layers for MLPs.
 
-Most of the time these will be built on MLPs, which will have an fprop
-method. Do we need to consider the case of an ensemble autoencoder, for
-example?
-
-Ensemble should behave something like an MLP, except with horizontal
-stacking as well as vertical stacking. We could even allow the underlying
-models to be mutable as in a MLP.
+If multiple models have been trained to predict the same targets, it is
+often useful to combine their output to improve predictions, possibly by
+learning a meta-estimator. The classes in this module are MLP layers that
+wrap CompositeLayer. While CompositeLayers can be used in MLPs without any
+wrapper (by flattening their output), these classes explicitly acknowledge
+and take advantage of the relationships between the predictions of the
+parallel layers.
 """
 from theano import tensor as T
 
-from pylearn2.models import Model
+from pylearn2.models.mlp import Layer
+
+__author__ = "Steven Kearnes"
+__copyright__ = "Copyright 2014, Stanford University"
+__license__ = "3-clause BSD"
 
 
-class Ensemble(Model):
+class Ensemble(Layer):
     """
-    Ensemble model that combines predictions from two or more models.
-    Subclasses perform different combinations, possibly learning a meta-
-    model.
+    Ensemble layer that combines output from each layer in a
+    CompositeLayer.
 
     Parameters
     ----------
-    models : list
-        Pretrained models.
+    composite_layer : CompositeLayer
+        Composite layer containing layers to run in parallel.
+    layer_name : str
+        Name of this layer.
     kwargs : dict
-        Keyword arguments for Model constructor.
+        Keyword arguments for `Layer` constructor.
     """
-    def __init__(self, models, **kwargs):
+    def __init__(self, composite_layer, layer_name, **kwargs):
         super(Ensemble, self).__init__(**kwargs)
-        self.models = models
+        self.composite_layer = composite_layer
+        self.layer_name = layer_name
 
-    def fprop(self, state):
+    def fprop(self, state_below):
         """
         Get predictions from each model in the ensemble.
 
         Parameters
         ----------
-        state : Space
-            Batch of examples to propogate through each model.
+        state_below : Space
+            Batch of examples to propogate through the composite layer.
         """
-        rval = [model.fprop(state) for model in self.models]
-        return rval
+        raise NotImplementedError('fprop')
 
 
 class EnsembleAverage(Ensemble):
     """
-    Ensemble model that calculates a (weighted) average of the predictions
-    of two or more models.
+    Ensemble layer that calculates a (weighted) average of the output from
+    the composite layer.
 
     Parameters
     ----------
-    models : list
-        Pretrained models.
+    composite_layer : CompositeLayer
+        Composite layer containing layers to run in parallel.
+    layer_name : str
+        Name of this layer.
     weights : list or None
         Contribution of each model in the weighted average.
     normalize : bool
         Whether to normalize averaged predictions so they sum to one.
     kwargs : dict
-        Keyword arguments for Model constructor.
+        Keyword arguments for `Layer` constructor.
     """
-    def __init__(self, models, weights, normalize=True, **kwargs):
-        super(EnsembleAverage, self).__init__(models, **kwargs)
+    def __init__(self, composite_layer, layer_name, weights=None,
+                 normalize=True, **kwargs):
+        super(EnsembleAverage, self).__init__(composite_layer, layer_name,
+                                              **kwargs)
         if weights is None:
-            weights = [1. for _ in models]
+            weights = [1.0 for _ in composite_layer.layers]
         self.weights = weights
         self.normalize = normalize
 
-    def fprop(self, state):
+    def fprop(self, state_below):
         """
-        Average model predictions.
+        Average outputs from composite layer.
 
         Parameters
         ----------
-        state : Space
+        state_below : Space
             Batch of examples to propogate through each model.
         """
-        rval = super(EnsembleAverage, self).fprop(state)
+        rval = self.composite_layer.fprop(state_below)
         rval = T.mean(rval, axis=0)
         if self.normalize:
             rval /= rval.sum(axis=1).dimshuffle(0, 'x')
