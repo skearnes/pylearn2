9aeccd37e33670d8744fa70812d22d22b1684930
skearnes <kearnes@stanford.edu>
2014-05-15 19:37:29 -0700

    WIP

--- pylearn2/models/ensemble.py
+++ pylearn2/models/ensemble.py
@@ -12,11 +12,19 @@ parallel layers.
 from theano import tensor as T
 
 from pylearn2.models.mlp import CompositeLayer
+from pylearn2.utils import safe_zip
 
 __author__ = "Steven Kearnes"
 __copyright__ = "Copyright 2014, Stanford University"
 __license__ = "3-clause BSD"
 
+
+def get_z_from_p(p):
+    """
+    Back out softmax inputs from probabilities. This is required because
+    the Softmax cost function checks that Y_hat is coming from softmax and
+    uses the raw inputs when calculating log probabilities.
+    """
 
 class Ensemble(CompositeLayer):
     """
@@ -32,21 +40,15 @@ class Ensemble(CompositeLayer):
         Name of this layer.
     layers : tuple or list
         The component layers to run in parallel.
+    probabilities : bool
+        Assume that component layer outputs are class probabilities.
     inputs_to_layers : dict or None
         Mapping for inputs to component layers.
     """
-    def set_output_space(self, space):
-        """
-        Set the output space of this layer. This method is provided for
-        subclasses that might not populate the same output space as their
-        component layers.
-
-        Parameters
-        ----------
-        space : Space
-            Output space.
-        """
-        self.output_space = space
+    def __init__(self, layer_name, layers, probabilities=True,
+                 inputs_to_layers=None):
+        super(Ensemble, self).__init__(layer_name, layers, inputs_to_layers)
+        self.probabilities = probabilities
 
     def set_input_space(self, space):
         """
@@ -66,6 +68,32 @@ class Ensemble(CompositeLayer):
             assert layer.get_output_space() == output_space
         self.set_output_space(output_space)
 
+    def set_output_space(self, space):
+        """
+        Set the output space of this layer. This method is provided for
+        subclasses that might not populate the same output space as their
+        component layers.
+
+        Parameters
+        ----------
+        space : Space
+            Output space.
+        """
+        self.output_space = space
+
+    def cost(self, Y, Y_hat):
+        """
+        Calculate the cost of predicting Y_hat when the true value is Y.
+
+        Parameters
+        ----------
+        Y : theano.gof.Variable
+            Target value(s).
+        Y_hat : theano.gof.Variable
+            Predicted value(s).
+        """
+        raise NotImplementedError('cost')
+
 
 class EnsembleAverage(Ensemble):
     """
@@ -80,20 +108,19 @@ class EnsembleAverage(Ensemble):
         The component layers to run in parallel.
     weights : list or None
         Contribution of each model in the weighted average.
-    normalize : bool
-        Whether to normalize averaged predictions so they sum to one.
+    probabilities : bool
+        Assume that component layer outputs are class probabilities.
     inputs_to_layers : dict or None
         Mapping for inputs to component layers.
     """
-    def __init__(self, layer_name, layers, weights=None, normalize=True,
+    def __init__(self, layer_name, layers, weights=None, probabilities=True,
                  inputs_to_layers=None):
         super(EnsembleAverage, self).__init__(layer_name, layers,
-                                              inputs_to_layers)
+                                              probabilities, inputs_to_layers)
         if weights is None:
             weights = [1.0 for _ in layers]
         assert len(weights) == len(layers)
         self.weights = weights
-        self.normalize = normalize
 
     def fprop(self, state_below):
         """
@@ -105,8 +132,24 @@ class EnsembleAverage(Ensemble):
             Batch of examples to propogate through each model.
         """
         components = super(EnsembleAverage, self).fprop(state_below)
-        components = tuple(w * c for w, c in zip(self.weights, components))
-        rval = T.mean(components, axis=0)
-        if self.normalize:
+        components = tuple(w * c
+                           for w, c in safe_zip(self.weights, components))
+        rval = T.sum(components, axis=0) / T.sum(self.weights)
+        if self.probabilities:
             rval /= rval.sum(axis=1).dimshuffle(0, 'x')
+        return rval
+
+    def cost(self, Y, Y_hat):
+        """
+        Average of component layer costs.
+
+        Parameters
+        ----------
+        Y : theano.gof.Variable
+            Target value(s).
+        Y_hat : theano.gof.Variable
+            Predicted value(s).
+        """
+        rval = tuple(w * layer.cost(Y, Y_hat)
+                     for w, layer in safe_zip(self.weights, self.layers))
         return rval
--- pylearn2/models/tests/test_ensemble.py
+++ pylearn2/models/tests/test_ensemble.py
@@ -4,10 +4,14 @@ Tests for ensemble models.
 from pylearn2.config import yaml_parse
 
 
-def test_ensemble_average():
-    """Test EnsembleAverage."""
-    trainer = yaml_parse.load(test_ensemble_average_yaml)
+def test():
+    trainer = yaml_parse.load(test_yaml)
     trainer.main_loop()
+
+#def test_ensemble_average():
+#    """Test EnsembleAverage."""
+#    trainer = yaml_parse.load(test_ensemble_average_yaml)
+#    trainer.main_loop()
 
 test_ensemble_average_yaml = """
 !obj:pylearn2.train.Train {
@@ -61,6 +65,43 @@ test_ensemble_average_yaml = """
             #    n_classes: 3,
             #    irange: 0.0,
             #},
+        ],
+    },
+    algorithm: !obj:pylearn2.training_algorithms.bgd.BGD {
+        batch_size: 10,
+        line_search_mode: exhaustive,
+        conjugate: 1,
+        termination_criterion:
+            !obj:pylearn2.termination_criteria.EpochCounter {
+                    max_epochs: 1,
+        },
+        monitoring_dataset: *train,
+    },
+}
+"""
+
+test_yaml = """
+!obj:pylearn2.train.Train {
+    dataset: &train
+        !obj:pylearn2.testing.datasets.random_one_hot_dense_design_matrix {
+            rng: !obj:numpy.random.RandomState { seed: 1 },
+            num_examples: 100,
+            dim: 10,
+            num_classes: 3,
+        },
+    model: !obj:pylearn2.models.mlp.MLP {
+        nvis: 10,
+        layers: [
+            !obj:pylearn2.models.mlp.Sigmoid {
+                layer_name: h0,
+                dim: 20,
+                irange: 0.05,
+            },
+            !obj:pylearn2.models.mlp.Softmax {
+                layer_name: y,
+                n_classes: 3,
+                irange: 0.0,
+            },
         ],
     },
     algorithm: !obj:pylearn2.training_algorithms.bgd.BGD {
