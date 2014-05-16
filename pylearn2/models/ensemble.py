71d8cee3d58b717bbec522aa3b3440da9b8596cb
skearnes <kearnes@stanford.edu>
2014-05-16 11:06:38 -0700

    Simplify

--- pylearn2/models/ensemble.py
+++ pylearn2/models/ensemble.py
@@ -11,20 +11,12 @@ parallel layers.
 """
 from theano import tensor as T
 
-from pylearn2.models.mlp import CompositeLayer
-from pylearn2.utils import safe_zip
+from pylearn2.models.mlp import CompositeLayer, MLP, Softmax
 
 __author__ = "Steven Kearnes"
 __copyright__ = "Copyright 2014, Stanford University"
 __license__ = "3-clause BSD"
 
-
-def get_z_from_p(p):
-    """
-    Back out softmax inputs from probabilities. This is required because
-    the Softmax cost function checks that Y_hat is coming from softmax and
-    uses the raw inputs when calculating log probabilities.
-    """
 
 class Ensemble(CompositeLayer):
     """
@@ -40,15 +32,25 @@ class Ensemble(CompositeLayer):
         Name of this layer.
     layers : tuple or list
         The component layers to run in parallel.
-    probabilities : bool
-        Assume that component layer outputs are class probabilities.
     inputs_to_layers : dict or None
         Mapping for inputs to component layers.
     """
-    def __init__(self, layer_name, layers, probabilities=True,
-                 inputs_to_layers=None):
+    def __init__(self, layer_name, layers, inputs_to_layers=None):
         super(Ensemble, self).__init__(layer_name, layers, inputs_to_layers)
-        self.probabilities = probabilities
+
+        # check that component layer output classes match
+        if isinstance(layers[0], MLP):
+            output_type = type(layers[0].layers[-1])
+            for layer in layers[1:]:
+                assert isinstance(layer.layers[-1], output_type)
+        else:
+            raise NotImplementedError('Ensemble layers must be MLPs.')
+        self.output_type = output_type
+
+        # check that component layer output spaces match
+        output_space = layers[0].get_output_space()
+        for layer in layers[1:]:
+            assert layer.get_output_space() == output_space
 
     def set_input_space(self, space):
         """
@@ -64,8 +66,6 @@ class Ensemble(CompositeLayer):
         """
         super(Ensemble, self).set_input_space(space)
         output_space = self.layers[0].get_output_space()
-        for layer in self.layers:
-            assert layer.get_output_space() == output_space
         self.set_output_space(output_space)
 
     def set_output_space(self, space):
@@ -97,8 +97,8 @@ class Ensemble(CompositeLayer):
 
 class EnsembleAverage(Ensemble):
     """
-    Ensemble layer that calculates a (weighted) average of the output from
-    the composite layer.
+    Ensemble layer that outputs the average of the output from the
+    composite layer.
 
     Parameters
     ----------
@@ -113,15 +113,6 @@ class EnsembleAverage(Ensemble):
     inputs_to_layers : dict or None
         Mapping for inputs to component layers.
     """
-    def __init__(self, layer_name, layers, weights=None, probabilities=True,
-                 inputs_to_layers=None):
-        super(EnsembleAverage, self).__init__(layer_name, layers,
-                                              probabilities, inputs_to_layers)
-        if weights is None:
-            weights = [1.0 for _ in layers]
-        assert len(weights) == len(layers)
-        self.weights = weights
-
     def fprop(self, state_below):
         """
         Average outputs from composite layer.
@@ -132,11 +123,10 @@ class EnsembleAverage(Ensemble):
             Batch of examples to propogate through each model.
         """
         components = super(EnsembleAverage, self).fprop(state_below)
-        components = tuple(w * c
-                           for w, c in safe_zip(self.weights, components))
-        rval = T.sum(components, axis=0) / T.sum(self.weights)
-        if self.probabilities:
-            rval /= rval.sum(axis=1).dimshuffle(0, 'x')
+        rval = T.mean(components, axis=0)
+        if issubclass(self.output_type, Softmax):
+            z = T.log(rval)
+            rval = T.nnet.softmax(z)
         return rval
 
     def cost(self, Y, Y_hat):
@@ -150,6 +140,6 @@ class EnsembleAverage(Ensemble):
         Y_hat : theano.gof.Variable
             Predicted value(s).
         """
-        rval = tuple(w * layer.cost(Y, Y_hat)
-                     for w, layer in safe_zip(self.weights, self.layers))
+        components = tuple(layer.cost(Y, Y_hat) for layer in self.layers)
+        rval = T.mean(components, axis=0)
         return rval
